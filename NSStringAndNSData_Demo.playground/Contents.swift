266d4eb0e38fa194c3376d74470e406c324f8134
Artem Lyksa <artemlyksa@gmail.com>
2016-04-09 12:54:08 +0300

    1

--- NSStringAndNSData_Demo.playground/Contents.swift
+++ NSStringAndNSData_Demo.playground/Contents.swift
@@ -1,5 +1,38 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+let fileManager = NSFileManager.defaultManager()
+
+do {
+    var documents = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+    print(documents)
+    var url = NSURL(string: "quote.txt", relativeToURL: documents)
+    let quote = "Four score and sever seven years ago"
+    if let url = url {
+        try quote.writeToURL(url, atomically: true, encoding: NSUTF8StringEncoding)
+        var text = try String(contentsOfURL: url)
+    }
+} catch {
+    print("error:\(error)")
+}
+
+if let coverAlbum = UIImage(named:"TSj6QWERGUI.jpg") {
+    let imageData = UIImagePNGRepresentation(coverAlbum)
+
+    do {
+        
+        var documents = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+        
+        var url = NSURL(string: "TSj6QWERGUI.bin", relativeToURL: documents)
+        
+        imageData!.writeToURL(url!, atomically: true)
+        
+        if let imageData = NSData(contentsOfURL: url!) {
+            let coverImage = UIImage(data: imageData)
+        }
+        
+    }catch{
+        print("error \(error)")
+    }
+}
+
+
